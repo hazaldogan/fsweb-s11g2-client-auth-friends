8cd92687ecf36478adac22477ec047107e3a2bfb
Hazal Taştan <hazl.dogann@gmail.com>
2023-11-01 15:33:32 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import "./App.css";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+
+import FriendsList from "./components/FriendsList";
+import AddFriend from "./components/AddFriend";
+import Friend from "./components/Friend";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Client Auth Projesi: Friends</h1>
+      <Header />
+      <div className=" content">
+        <Switch>
+          <PrivateRoute path="/friends/add/">
+            <AddFriend />
+          </PrivateRoute>
+          <PrivateRoute path="/friends/:friendID">
+            <Friend />
+          </PrivateRoute>
+          <PrivateRoute path="/friends/">
+            <FriendsList />
+          </PrivateRoute>
+          <Route path="/logout/">
+            <div>Logout</div>
+          </Route>
+          <Route path="/login/">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
